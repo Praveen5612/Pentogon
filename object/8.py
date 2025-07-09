777277a07013895490fc388b1a68b883759c4f6c
Praveen Reddy <praveenreddyg5612@gmail.com>
2025-07-09 12:09:22 +0530

    update

---
+++ object/8.py
@@ -0,0 +1,22 @@
+class car:
+    def __init__(self):
+        self.brand = "Toyato"
+        self.color = 'Pure Blaack'
+    def speed(a):
+        print("speed of vehicle is around 300")
+        a.cost = 25000000
+    def skill(b):
+        print('skill')
+        b.model = 'sy'
+
+c = car()
+c.skill()
+print(c.brand)
+print(c.color)
+c.speed()
+print(c.cost)
+
+c.model = 'Supra'
+print(c.model)
+
+    
